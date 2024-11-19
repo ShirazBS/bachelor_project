0e0578643ff180174fb721ef04849a679d8bed96
ShirazBS <shirazdbs@gmail.com>
2024-11-19 16:54:32 +0100

    continued working

---
+++ setup.sh
@@ -0,0 +1,9 @@
+!/usr/bin/env bash
+
+# any python packages you want to have preinstalled
+pip install pingouin
+pip install lxml
+
+# your git config information
+git config --global user.email "shirazdbs@gmail.com"
+git config --global user.name "ShirazBS"
